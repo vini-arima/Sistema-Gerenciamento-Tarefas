2c3abaa776821cfd26ae6321743c9040c9c85673
Vinícius Cardoso <viniciuscarimateia@gmail.com>
2026-02-07 21:38:12 -0300

    Adicionando getter no arquivo Model

--- src/com/model/TaskModel.java
+++ src/com/model/TaskModel.java
@@ -1,5 +1,30 @@
 package com.model;
 
 public class TaskModel {
+    String titulo;
+    String descricao;
+    int id;
+    boolean status;
 
+    public TaskModel(String titulo, String descricao, int id, boolean status) {
+        this.titulo = titulo;
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public int getId() {
+        return id; // CÓDIGO DA TAREFA
+    }
+
+    public boolean getStatus() {
+        return status; // FEITO OU NÃO FEITO
+    }
 }
