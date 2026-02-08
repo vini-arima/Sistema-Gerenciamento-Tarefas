ac92b564d6fee9917fa9e599c727ca42e7bf6d0c
Vinícius Cardoso <viniciuscarimateia@gmail.com>
2026-02-07 22:03:38 -0300

    Adicionando metodos e imports para o Arquivo View

--- src/com/view/TaskView.java
+++ src/com/view/TaskView.java
@@ -1,5 +1,23 @@
 package com.view;
 
+import java.util.Scanner;
+import com.model.TaskModel;
+
 public class TaskView {
 
+    public void adicionarTarefa() {
+
+    }
+
+    public void listarTarefa() {
+
+    }
+
+    public void atualizarTarefa() {
+
+    }
+
+    public void excluirTarefa() {
+
+    }
 }
