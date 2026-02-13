18ae2d456a894b9f46ba09b6bb3e35dbe5c37a1c
Vinícius Cardoso <viniciuscarimateia@gmail.com>
2026-02-13 15:51:51 -0300

    Adicionando função CREATE E READ no TaskRepository

--- src/com/repository/TaskRepository.java
+++ src/com/repository/TaskRepository.java
@@ -10,4 +10,24 @@ import java.util.List;
 public class TaskRepository {
     private static final String FILE_PATH = "dados.json";
 
+    // CREATE : Adiciona o novo objeto
+    public void create(int id, String nome) throws IOException {
+        JSONArray lista = readAll();
+        JSONObject novo = new JSONObject();
+        novo.put("id", id);
+        novo.put("nome", nome);
+        lista.put(novo);
+        save(lista);
+    }
+
+    // READ : ler todo o arquivo
+    public JSONArray readAll() {
+        // String conteudo = new String(Files.readAllBytes(Paths.get(FILE_PATH))) ; -
+        // depois
+        return new JSONArray(conteudo);
+    }
+
+    // UPDATE: Altera um registro pelo ID
+
+    // DELETE: Remove um registro pelo ID
 }
