f1da2a453f7c456ac502e0c5dda008ab5c3764a0
Vinícius Cardoso <viniciuscarimateia@gmail.com>
2026-02-13 13:54:32 -0300

    Configurar lib externa JSON

--- src/com/repository/TaskRepository.java
+++ src/com/repository/TaskRepository.java
@@ -1,5 +1,13 @@
 package com.repository;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.nio.file.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskRepository {
+    private static final String FILE_PATH = "dados.json";
 
 }
